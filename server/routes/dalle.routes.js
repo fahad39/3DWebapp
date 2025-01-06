a93e48632706e6d8373d144aa497a318b0ea3bd2
fahad39 <fahadhussain0127@gmail.com>
2025-01-06 22:39:35 +0500

    ADDNING routers

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello again" });
+});
+
+export default router;
