4618cce416990ec5233726f4b61e8687fff00c61
fahad39 <fahadhussain0127@gmail.com>
2025-01-07 23:20:24 +0500

    ai picker

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
-const AIPicker = () => {
-  return <div>AIPicker</div>;
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI...."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI"
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              customStyles="text-us"
+              handleClick={() => handleSubmit("Logo")}
+            />
+
+            <CustomButton
+              type="Filled"
+              title="AI Full"
+              customStyles="text-us"
+              handleClick={() => handleSubmit("full")}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AIPicker;
