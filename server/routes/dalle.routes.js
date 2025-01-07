ace3ff3df7e00edbf18fd2bc8fe9bdf00330847f
fahad39 <fahadhussain0127@gmail.com>
2025-01-07 23:06:00 +0500

    added server side code

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -6,8 +6,31 @@ dotenv.config();
 
 const router = express.Router();
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello again" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    return res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
 export default router;
